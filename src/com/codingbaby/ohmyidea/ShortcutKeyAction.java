f6c7146bbfd724b2ca40c5ca916c550bde09bebc
vsmysee <vsmysee@gmail.com>
2016-01-21 14:25:50 +0800

    fix the idea15 esc issue

--- src/com/codingbaby/ohmyidea/ShortcutKeyAction.java
+++ src/com/codingbaby/ohmyidea/ShortcutKeyAction.java
@@ -3,6 +3,8 @@ package com.codingbaby.ohmyidea;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbAware;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -38,6 +40,19 @@ public class ShortcutKeyAction extends AnAction implements DumbAware  {
         }
     }
 
+
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        final Editor editor =  e.getData(PlatformDataKeys.EDITOR);
+        if(editor == null){
+            e.getPresentation().setEnabled(false);
+        }else{
+            final KeyStroke keyStroke = getKeyStroke(e);
+            e.getPresentation().setEnabled(keyStroke != null);
+        }
+
+    }
 
     public static AnAction getInstance() {
         return ActionManager.getInstance().getAction(ACTION_ID);
