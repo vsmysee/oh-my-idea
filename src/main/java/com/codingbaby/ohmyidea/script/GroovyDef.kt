3aaacc323d7f94f98151ed841cfef0fb78dde46b
vsmysee <vsmysee@gmail.com>
2017-01-06 18:23:02 +0800

    update

---
+++ src/main/java/com/codingbaby/ohmyidea/script/GroovyDef.kt
@@ -0,0 +1,101 @@
+package com.codingbaby.ohmyidea.script
+
+object GroovyDef {
+
+    var groovy = """
+
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+class RobotContainer {
+
+        def outerList
+
+        RobotContainer(env){
+            outerList = env
+        }
+
+        def key(key,list) {
+            outerList[key] = list
+        }
+}
+
+
+class CodeContainer {
+
+        def outerList
+
+        def holder
+
+        CodeContainer(env){
+            outerList = env
+        }
+
+        def key(key,desc,code) {
+            outerList << ["key":key,"desc":desc,"code":code]
+        }
+
+}
+
+class ActorContainer {
+
+        def outerAction
+
+
+        ActorContainer(env){
+            outerAction = env
+        }
+
+
+        def key(key,action,desc) {
+            outerAction.add(key, action, desc)
+        }
+}
+
+
+def code_snippet = {
+    closure ->
+        closure.delegate = new CodeContainer(envList)
+        closure()
+}
+
+def app_quick_open = {
+    closure ->
+        closure.delegate = new RobotContainer(envMap)
+        closure()
+}
+
+def keyboard_select = {
+    closure ->
+        closure.delegate = new ActorContainer(vmode)
+        closure()
+}
+
+def keyboard_single = {
+    closure ->
+        closure.delegate = new ActorContainer(smode)
+        closure()
+}
+
+def keyboard_movement = {
+    closure ->
+        closure.delegate = new ActorContainer(mmode)
+        closure()
+}
+
+def keyboard_composite = {
+    closure ->
+        closure.delegate = new ActorContainer(cmode)
+        closure()
+}
+
+def keyboard_bottom = {
+    closure ->
+        closure.delegate = new ActorContainer(bmode)
+        closure()
+}
+
+
+"""
+
+}
--- src/main/java/com/codingbaby/ohmyidea/script/OhScript.kt
+++ src/main/java/com/codingbaby/ohmyidea/script/OhScript.kt
@@ -14,102 +14,6 @@ object OhScript {
 
     val OH_FILE = ".oh-my-idea"
 
-    val dsl = """
-
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
-
-class RobotContainer {
-
-        def outerList
-
-        RobotContainer(env){
-            outerList = env
-        }
-
-        def key(key,list) {
-            outerList[key] = list
-        }
-}
-
-
-class CodeContainer {
-
-        def outerList
-
-        def holder
-
-        CodeContainer(env){
-            outerList = env
-        }
-
-        def key(key,desc,code) {
-            outerList << ["key":key,"desc":desc,"code":code]
-        }
-
-}
-
-class ActorContainer {
-
-        def outerAction
-
-
-        ActorContainer(env){
-            outerAction = env
-        }
-
-
-    def key(key,action,desc) {
-            outerAction.add(key, action, desc)
-    }
-
-
-}
-
-def code_snippet = {
-    closure ->
-        closure.delegate = new CodeContainer(envList)
-        closure()
-}
-
-def app_quick_open = {
-    closure ->
-        closure.delegate = new RobotContainer(envMap)
-        closure()
-}
-
-def keyboard_select = {
-    closure ->
-        closure.delegate = new ActorContainer(vmode)
-        closure()
-}
-
-def keyboard_single = {
-    closure ->
-        closure.delegate = new ActorContainer(smode)
-        closure()
-}
-
-def keyboard_movement = {
-    closure ->
-        closure.delegate = new ActorContainer(mmode)
-        closure()
-}
-
-def keyboard_composite = {
-    closure ->
-        closure.delegate = new ActorContainer(cmode)
-        closure()
-}
-
-def keyboard_bottom = {
-    closure ->
-        closure.delegate = new ActorContainer(bmode)
-        closure()
-}
-
-"""
-
 
     fun loadScriptFile() {
         if (try {
@@ -128,7 +32,7 @@ def keyboard_bottom = {
 
     fun loadGroovy(text: String) {
         val groovyClassLoader = GroovyClassLoader()
-        val scriptClass = groovyClassLoader.parseClass(dsl + text)
+        val scriptClass = groovyClassLoader.parseClass(GroovyDef.groovy + text)
 
         var keyHolder = ArrayList<HashMap<String, String>>()
         var robotHolder = HashMap<String, List<Int>>()
