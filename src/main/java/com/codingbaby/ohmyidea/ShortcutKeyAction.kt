ad24dfd3fcb2c8302cf66ae64f178ed1b3bad12d
vsmysee <vsmysee@gmail.com>
2017-02-27 16:25:20 +0800

    update

--- src/main/java/com/codingbaby/ohmyidea/ShortcutKeyAction.kt
+++ src/main/java/com/codingbaby/ohmyidea/ShortcutKeyAction.kt
@@ -13,7 +13,7 @@ import javax.swing.KeyStroke
  */
 class ShortcutKeyAction : AnAction(), DumbAware {
 
-    var timeRange = 100
+    var timeRange = 90
 
     override fun actionPerformed(anActionEvent: AnActionEvent) {
 
